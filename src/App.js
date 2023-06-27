5c98c48e3cd6df25e7f6d3aa500439bf3642f0b6
Phan Cong Hieu <phanhieu2572002@gmail.com>
2023-06-27 18:37:40 +0700

    calendar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/login/login";
 import Home from "./pages/home/home";
 import Task from "./pages/task/task";
+import TaskCalendar from "./pages/calendar/calendar";
 function App() {
   return (
     <div>
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/tasks" element={<Task />} />
+        <Route path="/tasks/calendar" element={<TaskCalendar />} />
       </Routes>
     </div>
   );
